a12625e9f01505bb2b4c8190da3b3cfcc477e8f3
sooguan <sooguan@elixirtech.com>
2024-12-12 10:18:50 +0800

    Develop (#2)
    * add image version to environment variable.
    
    * export environment variable

--- run-ambience-compose.sh
+++ run-ambience-compose.sh
@@ -1,4 +1,6 @@
-docker pull elixirtech/elixir-ambience
+version=2025.0
+export version
+docker pull elixirtech/elixir-ambience:${version}
 #variables are in .env file
 docker compose down 
 docker compose up -d 
